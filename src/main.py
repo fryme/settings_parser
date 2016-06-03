e7c6a4b7d0381f8d0dc22d1fcc39818c7c3d497c
fryme <oleg.fry@gmail.com>
2016-06-03 18:58:27 +0300

    New approach: let's try to create a sequence of objects, like list

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 __author__ = 'pelipenko'
-
-import re as re
+import re
+import fileinput
 
 class Line:
-    prefix = ''
     key = ''
     value = ''
     postfix = ''
@@ -15,41 +14,22 @@ class Line:
          return "__unicode__"
 
     def __repr__(self):
-         return 'prefix:\'{0}\' key:\'{1}\' value:\'{2}\' postfix:\'{3}\'' .format(self.prefix, self.key, self.value, self.postfix)
+         return 'prefix:\'{0}\' key:\'{1}\' value:\'{2}\' postfix:\'{3}\'' .format(self.key, self.value, self.postfix)
 
 class Parser:
-    def splitLine(self, line):
+    def split_line(self, line):
         if line.isspace():
             return None
-        '''
-        res = re.search('^(?P<comment>[#;].*)', line)
-        l = Line
-        if None != res.group('comment'):
-            l.prefix = res.group('comment')
-        else:
-            res = re.search('^(?P<key>([\\S]+))', line)
-            if None != res.group('key'):
-                l.key = res.group('key')
 
-            res = re.search('^(?P<value>([\\S]+))', line)
-            lst = list(line)
-            #del lst[0:2]
-            #lst.insert(2, '11111')
-            return ''.join(lst)
-
-        return l
-        '''
         lst = list(line.rstrip('\n'))
 
         l = Line()
         isComment = False
-        sss = ''
-        #rc = re.compile(".*\s+(?=[A-Z#])(?!.\s)").split(line)
-        #rc = re.findall("\\b[a-z #']*\\w+", line) #.split(line)
-        found = re.findall("\\s*[\S]*\S", line)
+        #found = re.findall("\\s*[\S]*\S", line)
+        found = re.compile("^\s*(\b[a-zA-Z0-9]+)\s*", line)
 
-        #print 'f', found
-        if found is not None:
+        print 'f', found
+        if len(found) != 0:
             key = found[0]
             if found[0][0] is '#': #comment detected
                 return None #l.prefix = ''.join(rc)
@@ -62,55 +42,211 @@ class Parser:
                 l.value = ''.join(found[1:idx])
                 if idx != len(found):
                     l.postfix = ''.join(found[idx:])
-        '''
-        sp = line.split()
-        if sp[0][0] == '#':
-            l.prefix = ''.join(sp)
-        else:
-            counter = 0
-            l.key = sp[0]
-            postfix = ''
-
-            for word in sp:
-                if word[0] == '#':
-                    l.postfix = sp[counter:]
-                else:
-                    word
-                counter += 1
-
-        return l
-        '''
         return l
 
+    def generateLine(self, line = Line()):
+        return '{0} {1} {2}'.format(line.key, line.value, line.postfix)
 
 class SettingsParser:
-
     def __init__(self, filename):
-        self._file = open(filename, 'r')
+        self._filename = filename
         self._parser = Parser()
         self._settingsDict = dict()
+        self._keyValueDict = dict()
+    '''
+    def memory_read_all(self):
+        with open(self._filename, 'r') as file:
+            for idx, line in enumerate(file):
 
-    def readAll(self):
-        for idx, line in enumerate(self._file):
-            lineObj = self._parser.splitLine(line)
-            if lineObj is not None:
-                self._settingsDict[idx] = lineObj
-        return self._settingsDict
+    '''
+
+    def read_all(self):
+        with open(self._filename, 'r') as file:
+            for idx, line in enumerate(file):
+                line_obj = self._parser.split_line(line)
+                if line_obj is not None:
+                    self._settingsDict[idx] = line_obj
+                    self._keyValueDict[line_obj.key] = line_obj.value
+        return self._keyValueDict
 
     # replace if key exists
-    def set(self, key='', value=''):
+    def set(self, key='', value='', saveToFile = False):
+        self._keyValueDict[key] = value
+        if saveToFile:
+            self.save(key)
         return
 
+    '''
     def get(self, key='', value=''):
+        with open(self._filename, 'r') as file:
+            for idx, line in enumerate(file):
+        return
+    '''
+
+    def remove(self, key_to_remove='', remove_all_line = False):
+        key_was_removed = False
+        for line in fileinput.input(self._filename, inplace=True, mode='r'):
+            key_was_removed = False
+            strip_line = line.strip()
+            lineObj = self._parser.split_line(strip_line)
+            if lineObj is not None:
+                if lineObj.key == key_to_remove:
+                    if not remove_all_line:
+                        lineObj.key = ''
+                        lineObj.value = ''
+                        print self._parser.generateLine(lineObj)
+
+                    key_was_removed = True
+            if not key_was_removed:
+                print strip_line
+        '''
+        if not key_was_removed:
+            raise ValueError("Key was not found")
+        '''
+        # if keyToSave save only key
         return
 
-    def remove(self, key='', value=''):
+    def save(self, key_to_save='', value_to_save=''):
+        for line in fileinput.input(self._filename, inplace=True, mode='r'):
+            line_was_printed = False
+            strip_line = line.strip()
+            lineObj = self._parser.split_line(strip_line)
+            if lineObj is not None:
+                if lineObj.key == key_to_save:
+                    lineObj.value = value_to_save
+                    print self._parser.generateLine(lineObj)
+                    line_was_printed = True
+            if not line_was_printed:
+                print strip_line
+        # if keyToSave save only key
         return
 
-    def save(self):
+
+
+# let's try to create a sequence of objects, like list:
+#
+# key value for key #comment
+# # comment2
+#
+# key2
+#
+# Will transform to:
+# KeyValuePairWithComment("key", "value for key", "comment")
+# Comment("comment2")
+# EmptyString()
+# KeyValuePair("key2", "")
+# EmptyString()
+
+class Comment:
+    def __init__(self,text=''):
+        self.text = text
+    text = ''
+    def __repr__(self):
+        return "Comment:'{0}'".format(self.text)
+
+class KeyValue:
+    key = ''
+    value = ''
+
+class KeyValueWithComment:
+    key = ''
+    value = ''
+    comment = Comment
+    def __repr__(self):
+        return "KeyValuePairWithComment: '{0}' '{1}' '{2}'".format(self.key, self.value, self.comment.text)
+
+class EmptyString:
+    text = ''
+    def __repr__(self):
+        return "EmptyString: ''"
+
+class NotValidString:
+    text = ''
+    def __repr__(self):
+        return "NotValidString: '{0}'".format(self.text)
+
+
+class FileSerializer:
+    def _parse_line(self, line=''):
+        if line.isspace():
+            return EmptyString()
+
+        found = re.findall("\\s*[\S]*\S", line)
+
+        if len(found) != 0:
+            if found[0][0] is '#': #comment detected
+                return Comment(line.strip())
+            else:
+                pair = KeyValueWithComment()
+                pair.key = found[0].strip()
+                idx = 0
+                for idx, word in enumerate(found):
+                    if word.strip()[0] is '#':
+                        break
+                pair.value = ''.join(found[1:idx])
+                if idx != len(found):
+                    pair.comment = Comment(''.join(found[idx:]))
+
+                return pair
+        return NotValidString(line)
+
+    def read_file(self, filename):
+        serialized_object_list = list()
+        with open(filename, 'r') as file:
+            for line in file:
+                serialized_object_list.append(self._parse_line(line))
+
+        return serialized_object_list
+
+class SettingsParser2:
+    def __init__(self, filename):
+        self._filename = filename
+        serializer = FileSerializer()
+        serializer.read_file(filename)
+        self._settingsDict = dict()
+        self._keyValueDict = dict()
+
+    # returns list of KeyValues
+    # maybe return without comments?
+    def read_all_keys(self):
+        return
+
+    # returns KeyValue by key
+    # reads from end of file
+    #
+    # alg:
+    # open file, read line by line while didn't get our value
+    def get(self,key=''):
+        return
+
+    # replace KeyValue in file
+    # reads from end of file
+    #
+    # alg:
+    # open file, read line by line while didn't get our value
+    # when get replace value
+    # if not get add KeyValue to end of file
+    def set(self, key_value=KeyValue()):
+        return
+
+    # remove key with value from file
+    # reads from end of file
+    #
+    # alg:
+    # open file, read line by line while didn't get our value
+    # when found remove all line
+    def remove(self, key=''):
         return
 
 if __name__ == "__main__":
-    filename = '../examples/simple_config.txt'
-    parser = SettingsParser(filename)
-    print(parser.readAll())
+    pass
+    #filename = '../examples/simple_config.txt'
+    #parser = SettingsParser(filename)
+    #print parser.read_all()
+    #parser.remove('key1', True)
+    #print parser.read_all()
+    #parser.save('key1', 'HELLO! world orgjeoir e jgperojg perj gper ')
+    #parser.remove('key1', True)
+
+
+
